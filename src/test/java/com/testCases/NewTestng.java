09bb9b1ec7d90edeafcaba3ee7e8570d72518b28
tipuranjalid <109160586+tipuranjalid@users.noreply.github.com>
2023-10-25 16:01:46 +0530

    Update NewTestng.java

--- src/test/java/com/testCases/NewTestng.java
+++ src/test/java/com/testCases/NewTestng.java
@@ -1,6 +1,6 @@
 package com.testCases;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.Assert;
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
@@ -8,17 +8,17 @@ public class NewTestng {
   @Test(priority=1)
   public void test1() {
 	  System.out.println("test1");
-	  Assertions.assertEquals(1, 1);
+	  Assert.assertEquals(1, 1);
   }
   @Test(priority=2)
   public void test2() {
 	  System.out.println("test2");
-	  Assertions.assertEquals("A", "B");
+	  Assert.assertEquals("A", "B");
 	  
   }
   @Test(priority=3,dependsOnMethods= {"test2"})
   public void test3() {
 	  System.out.println("test3");
-	  Assertions.assertEquals(1, 1);
+	  Assert.assertEquals(1, 1);
   }
 }
