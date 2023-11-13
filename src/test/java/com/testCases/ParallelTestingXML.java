2937e3989d469cc3d3b18528b8e68de85e0585a4
tipuranjalid <109160586+tipuranjalid@users.noreply.github.com>
2023-11-13 10:58:39 +0530

    changed duration

--- src/test/java/com/testCases/ParallelTestingXML.java
+++ src/test/java/com/testCases/ParallelTestingXML.java
@@ -69,7 +69,7 @@ public class ParallelTestingXML extends BaseTest{
 		}
 		System.out.println(bcksps);
 		driver.findElement(By.xpath("//input[@name='lastName']")).clear();
-		Thread.sleep(Duration.ofSeconds(10));
+		Thread.sleep(Duration.ofSeconds(1000));
 		firstNameEle.click();
 		firstNameEle.sendKeys(bcksps);
 		firstNameEle.sendKeys("hhaaaaaaaaaaaaaaaaaaaa");
